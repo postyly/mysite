e5e09d761ce8fa288e35429cbe382ac9e5324578
Ivan Azarkevich <postyly@yandex.ru>
2017-02-03 17:22:58 +0300

    create admin password

--- admin/core/function.php
+++ admin/core/function.php
@@ -1,1 +1,11 @@
 <?php
+//соединение с базой данных
+const USER = 'admin';
+const DB_NAME = 'u272860375_admin';
+const PWD = 'rfcnthnhjq';
+const HOST = 'localhost';
+
+function db_connect()
+{
+    return mysqli_connect(HOST, USER, DB_NAME, DB_NAME);
+}
