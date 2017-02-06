5d9c0c79e34035c2de990d7050dc669c3c23ae96
Ivan Azarkevich <postyly@yandex.ru>
2017-02-07 00:02:26 +0300

    add soc network to contact

--- views/contact.views.php
+++ views/contact.views.php
@@ -5,8 +5,10 @@
         <p class="lead">Местонахождение: город Минск</p>
         <p class="lead">телефон: +37529 8653830</p>
         <p class="lead">e-mail: postyly@gmail.com</p>
+        <p class="lead"><a href="https://www.facebook.com/ivan.azarkevich">Facebook</a></p>
+        <p class="lead"><a href="https://ok.ru/profile/122572901594">OK.ru</a></p>
         <p class="lead">Вы можете отправить мне сообщение.</p>
-        <form action="index.php?page=contact" method="post">
+        <form action="index.php?page=contact" accept-charset="utf-8" method="post">
             <div class="form-group">
                 <textarea class="form-control" rows="3" placeholder="Текст сообщения" name="message_text"></textarea>
             </div>
