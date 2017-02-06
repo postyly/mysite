a14b21fac92e89c4a0b3244dfa8bf12e45a01c8e
Ivan Azarkevich <postyly@yandex.ru>
2017-02-06 15:56:08 +0300

    add table of hit and host in counter.views

--- admin/views/counter.views.php
+++ admin/views/counter.views.php
@@ -2,11 +2,16 @@
 //достаем из базы все данные из таблицы u_hit
 $link = db_connect();
 $query = "SELECT ip, UNIX_TIMESTAMP(`date_time`) FROM ip_stat";
-$result = mysqli_query($link, $query) or die('Ошибка БД');
+$result = mysqli_query($link, $query) or die('Ошибка запроса к базе данных');
 
+//достаем из базы данных статистику просмотров и уникальных посещений
+$query = "SELECT `date`, `host`, `hit` FROM hits";
+$result_hits = mysqli_query($link, $query) or die('Ошибка запроса к базе данных');
 ?>
 <h1>Статистика посещений</h1>
-<table border="1">
+<!--статистика ip аресов и время входа-->
+
+<table border="1" class="pull-left">
     <tr>
         <th>ip addres</th>
         <th>date</th>
@@ -17,4 +22,21 @@ $result = mysqli_query($link, $query) or die('Ошибка БД');
             <td><?= date("Y-m-d, H:i:s", $arr[1]) ?></td>
         </tr>
     <?php endwhile; ?>
+</table>
+<!--статистика уникальных посещений и просмотров-->
+<table border="1" class="pull-right">
+    <tr>
+        <th>дата</th>
+        <th>уникальных посещений</th>
+        <th>просмотров</th>
+    </tr>
+
+    <?php while ($arr = mysqli_fetch_row($result_hits)): ?>
+        <tr>
+            <td><?= $arr[0] ?></td>
+            <td><?= $arr[1] ?></td>
+            <td><?= $arr[2] ?></td>
+        </tr>
+    <?php endwhile; ?>
+
 </table>
