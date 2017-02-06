957c36e4f78cc91308842fcaf01efea51b7e7e81
Ivan Azarkevich <postyly@yandex.ru>
2017-02-06 18:27:24 +0300

    add project 2 in portfolio

---
+++ portfolio/forsergey/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Мрамор сибири.</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <!--font-->
+    <link href="https://fonts.googleapis.com/css?family=Play" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div class="container">
+    <div class="col-lg-2 col-md-1"></div>
+    <div class="col-lg-8 col-md-10 content">
+        <div class="col-lg-6 col-md-6">
+            <h1>Мрамор сибири</h1>
+            <p>Производим и продаем:</p>
+            <ul>
+                <li>Декоративные изделия</li>
+                <li>Посуда</li>
+                <li>Подарки</li>
+                <li>Светильник</li>
+                <li>Камины</li>
+                <li>Столешницы</li>
+            </ul>
+            <p>Из натурального природного камня</p>
+        </div>
+        <div class="col-lg-6 col-md-6">
+            <h3>+375(29)315-35-02</h3>
+            <p>Скачайте каталог дизайнерских решений из натурального камня в интерьере</p>
+            <form class="form-horizontal col-lg-10 col-md-10">
+                <div class="form-group">
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="user_name" placeholder="Имя">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control" id="user_email" placeholder="Email">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-sm-offset-4 col-sm-8">
+                        <button type="submit" class="btn btn-default">Отправить</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="col-lg-2 col-md-1"></div>
+
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
--- views/portfolio.views.php
+++ views/portfolio.views.php
@@ -2,5 +2,6 @@
     <h1 class="cover-heading">Мои работы:</h1>
     <ul>
         <li class="lead"><a href="../portfolio/f_proekt">Верстка PSD шаблона</a></li>
+        <li class="lead"><a href="../portfolio/forsergey">Для сергея</a></li>
     </ul>
 </div>
