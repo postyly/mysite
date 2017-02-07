36aea0c20cfe9e02571f68767a832774a07eeefa
Ivan Azarkevich <postyly@yandex.ru>
2017-02-07 16:53:04 +0300

    add atribut target in portfolio link and add background color in forsergey/css/style

--- views/portfolio.views.php
+++ views/portfolio.views.php
@@ -1,7 +1,7 @@
 <div class="inner cover">
     <h1 class="cover-heading">Мои работы:</h1>
     <ul>
-        <li class="lead"><a href="../portfolio/f_proekt">Верстка PSD шаблона</a></li>
-        <li class="lead"><a href="../portfolio/forsergey">Для сергея</a></li>
+        <li class="lead"><a href="../portfolio/f_proekt" target="_blank">Верстка PSD шаблона</a></li>
+        <li class="lead"><a href="../portfolio/forsergey" target="_blank">Для сергея</a></li>
     </ul>
 </div>
