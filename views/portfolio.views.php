b0bf2dc3cd4e2168bea54d66bab6818acbb9d5ae
Ivan Azarkevich <postyly@yandex.ru>
2017-02-24 15:36:42 +0300

    modified:   views/portfolio.views.php

--- views/portfolio.views.php
+++ views/portfolio.views.php
@@ -3,6 +3,11 @@
     <ul>
         <li class="lead"><a href="../portfolio/f_proekt" target="_blank">Верстка PSD шаблона</a></li>
         <li class="lead"><a href="../portfolio/forsergey" target="_blank">Для сергея</a></li>
-        <li class="lead"><a href="../portfolio/metal_calculator" target="_blank">Калькулятор металлопрокат</a></li>
+        <li class="lead"><a href="../portfolio/metal_calculator" target="_blank">Калькулятор металлопрокат</a>
+            <ul>
+                <li class="lead"><a href="https://orgmetall.ru/page/kalkulyator-metallov.html" target="_blank">Калькулятор работает на реальном
+                        проекте.</a></li>
+            </ul>
+        </li>
     </ul>
 </div>
