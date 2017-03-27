d707a332582bf7e528975482ebd0e82b725a193e
Ivan Azarkevich <postyly@yandex.ru>
2017-03-27 23:45:35 +0300

    add new course

--- views/main.views.php
+++ views/main.views.php
@@ -4,14 +4,18 @@
     <p class="lead">Цель: <br>
         Позиция junior php developer. </p>
     <p class="lead">Навыки: <br>
-        PHP, HTML, CSS, GIT, Bootstrap, MySQL</p>
+        PHP, HTML, CSS, GIT, Bootstrap, MySQL, Bitrix(контент-менеджжер)</p>
     <p class="lead">Опыт работы: <br>
         02.2014г - 03.2016г. <br>
         ОДО "Принттехника" - системный администратор</p>
     <p class="lead">Образование - высшее: <br>
         Инженер по эксплуатаии, <br> Военная академия РБ 1999 - 2003г.</p>
-    <p class="lead">Курсы повышения квалификации: <br>
-        Академия Белхард - PHP - базовый 2016г</p>
+    <p class="lead">Курсы повышения квалификации: </p>
+    <ul class="lead">
+        <li>Академия Белхард - "PHP - базовый" 2016г</li>
+        <li>ООО "1С-Битрикс" - "Контент - менеджер" 2017г</li>
+    </ul>
+
     <p class="lead">Уровень английского: <br>
         A1 Elementary<br>
     </p>
